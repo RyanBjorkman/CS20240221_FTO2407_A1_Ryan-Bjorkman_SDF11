41cd80b03f4c64be0aa056d0cacc48902f630c8d
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-08-15 16:38:29 +0200

    added presentation and loom links in readme.md

--- assets/js/loopple/loopple.js
+++ assets/js/loopple/loopple.js
@@ -6,3 +6,42 @@ document.querySelector('.loopple-alert.loopple-alert-dismissible .close').onclic
    document.querySelector('.loopple-alert').remove();
  }, 1000);
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const counters = document.querySelectorAll('[id^="countto"]');
+
+  const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5 // Trigger when 50% of the element is in view
+  };
+
+  const startCount = (counter) => {
+      const countTo = parseInt(counter.getAttribute('countto'));
+      const decimal = counter.getAttribute('data-decimal') || 0;
+      let currentCount = 0;
+      const increment = countTo / 100;
+
+      const countInterval = setInterval(() => {
+          currentCount += increment;
+          if (currentCount >= countTo) {
+              currentCount = countTo;
+              clearInterval(countInterval);
+          }
+          counter.textContent = currentCount.toFixed(decimal);
+      }, 20); // Speed of counting
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              startCount(entry.target);
+              observer.unobserve(entry.target);
+          }
+      });
+  }, options);
+
+  counters.forEach(counter => {
+      observer.observe(counter);
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -234,3 +234,42 @@ module.exports = {
   },
   plugins: ['prettier-plugin-tailwindcss'],
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const counters = document.querySelectorAll('[id^="countto"]');
+
+  const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5 // Trigger when 50% of the element is in view
+  };
+
+  const startCount = (counter) => {
+      const countTo = parseInt(counter.getAttribute('countto'));
+      const decimal = counter.getAttribute('data-decimal') || 0;
+      let currentCount = 0;
+      const increment = countTo / 100;
+
+      const countInterval = setInterval(() => {
+          currentCount += increment;
+          if (currentCount >= countTo) {
+              currentCount = countTo;
+              clearInterval(countInterval);
+          }
+          counter.textContent = currentCount.toFixed(decimal);
+      }, 20); // Speed of counting
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              startCount(entry.target);
+              observer.unobserve(entry.target);
+          }
+      });
+  }, options);
+
+  counters.forEach(counter => {
+      observer.observe(counter);
+  });
+});
